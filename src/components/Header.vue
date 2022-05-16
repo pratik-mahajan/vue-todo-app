4a06069894057cff04c78deaf256b5155dd7afb0
Pratik Mahajan <mahajanp.gurudatta.min19@itbhu.ac.in>
2022-05-16 23:12:57 +0530

    First Commit

---
+++ src/components/Header.vue
@@ -0,0 +1,23 @@
+<template>
+    <header>
+        <h1>{{title}}</h1>
+    </header>    
+</template>
+
+<script>
+export default {
+    name: "Header",
+    props: {
+        title: String,
+    }
+}
+</script>
+
+<style scoped>
+    header{
+        justify-content: space-between;
+        display: flex;
+        align-items: center;
+        margin-bottom: 20px;
+    }
+</style>
